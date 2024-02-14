371dbaa2025d50bd4825af4527eaf72202cde976
Greg Pomeroy <w0167705@campus.nscc.ca>
2024-02-14 16:07:58 -0400

    Initial commit: Initial Set up of default servlet, database connection, and default JSP. MySQL server has been set up and created through XAMPP.

---
+++ src/main/java/com/gregpomeroy/todoj2ee/webd3102_assignment1_gregpomeroy/HelloServlet.java
@@ -0,0 +1,59 @@
+package com.gregpomeroy.todoj2ee.webd3102_assignment1_gregpomeroy;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() throws ServletException {
+
+        // Initialize database connection
+        try {
+            Connection connection = DBConnect.getConnection();
+            System.out.println("Connected to the database");
+
+            // You can store the connection object in a servlet context attribute
+            getServletContext().setAttribute("dbConnection", connection);
+        } catch (SQLException e) {
+            throw new ServletException("Error connecting to the database", e);
+        }
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+        Connection connection = (Connection) getServletContext().getAttribute("dbConnection");
+        if (connection != null) {
+            try {
+                connection.close();
+                System.out.println("Database connection closed");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>J2EE To Do Application</title>
+</head>
+<body>
+<h1><%= "To Do Application" %>
+</h1>
+<br/>
+<a href="hello-servlet">Hello Servlet</a>
+</body>
+</html>
